7dc57cc734ec520533966facfe927664c83c1664
Vaishnani Amisha <vaishnani.amisha@eagleview.com>
2021-02-23 14:24:12 +0530

    fixed: UTs

--- api/userHandler_test.go
+++ api/userHandler_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"scripbox/hackathon/api"
 	"scripbox/hackathon/errors"
+	"scripbox/hackathon/model"
 	"scripbox/hackathon/service"
 	"testing"
 
@@ -20,27 +21,28 @@ func TestGetUserDetails(t *testing.T) {
 		expectedResult string
 		resStatus      int
 	}{
-		{"success", 1001, "1001", `{"employeeId":"1001","EmployeeName":"User 1"}`, http.StatusOK},
-		{"InvalidUserId", 0, "abc", `{"Code":400,"ErrorMessage":"Invalid Input: Please provide valid userID"}`, http.StatusBadRequest},
-		{"UserNotExist", 0, "0", `{"Code":400,"ErrorMessage":"UserId does not exist"}`, http.StatusNotFound},
+		{"success", 1001, "?userID=1001", `{"employeeId":1001,"employeeName":"User 1"}`, http.StatusOK},
+		{"EmptyUserId", 0, "", `{"Code":400,"ErrorMessage":"Invalid Input: Please provide userID"}`, http.StatusBadRequest},
+		{"InvalidUserId", 0, "?userID=abc", `{"Code":400,"ErrorMessage":"Invalid Input: Please provide valid userID"}`, http.StatusBadRequest},
+		{"UserNotExist", 0, "?userID=0", `{"Code":404,"ErrorMessage":"UserId does not exist"}`, http.StatusNotFound},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			mockUserService := new(service.MockUserService)
 			e := echo.New()
-			req := httptest.NewRequest(echo.GET, "/v1/hackathon/user/"+tt.userIDStr, nil)
+			req := httptest.NewRequest(echo.GET, "/v1/hackathon/user"+tt.userIDStr, nil)
 			rec := httptest.NewRecorder()
 			context := e.NewContext(req, rec)
 			handler := api.UserHandler{
 				UserService: mockUserService,
 			}
 			if tt.name == "UserNotExist" {
-				mockUserService.On("GetUserDetails", tt.userID).Return(&errors.ServiceError{
+				mockUserService.On("GetUserDetails", tt.userID).Return(model.User{}, &errors.ServiceError{
 					Code:         http.StatusNotFound,
 					ErrorMessage: "UserId does not exist",
 				})
 			} else {
-				mockUserService.On("GetUserDetails", tt.userID).Return((*errors.ServiceError)(nil))
+				mockUserService.On("GetUserDetails", tt.userID).Return(model.User{ID: 1001, Name: "User 1"}, (*errors.ServiceError)(nil))
 			}
 
 			err := handler.GetUserDetails(context)
